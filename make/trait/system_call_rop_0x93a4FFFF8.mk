ef3ea7f19df988d56acdd7ba381b6f5109dd1af5
VV1LD <wildcardbelcher@gmail.com>
2017-06-03 22:27:33 +0100

    Update system_call_rop_0x93a4FFFF8.mk

--- make/trait/system_call_rop_0x93a4FFFF8.mk
+++ make/trait/system_call_rop_0x93a4FFFF8.mk
@@ -4,5 +4,4 @@
 #Needed for resolving stubs.
 #This makes the elf dependant on a specific in-process setup.
 Libraries += -lPs4_base_assembler_system_call_rop_0x93a4FFFF8
-
 ###################################
